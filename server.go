05e3c61f707e02f67c20716475e1d15b38b3bbe4
Anurag <anuragtangri@Anurags-MacBook-Air.local>
2024-10-20 14:39:39 -0700

    my name is ANU

--- server.go
+++ server.go
@@ -46,7 +46,7 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/ws", handleWebSocket)
-
+	fmt.Println("my name is LOL")
 	fmt.Println("Server starting on :8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
